e04d9c8ecd42b3af17bdfb9cff1e4151c25ae70b
huseynovelmir <huseynovelmir789@gmail.com>
2023-07-10 19:40:49 +0400

    Navbar is done

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -6,6 +6,7 @@ import { logo, menu, close } from '../assets'
 
 const Navbar = () => {
   const [active, setActive] = useState("")
+  const [toggle, setToggle] = useState(false)
 
   return (
     <nav className={`${styles.paddingX} w-full flex items-center py-5 fixed top-0 z-20 bg-primary`}>
@@ -15,12 +16,56 @@ const Navbar = () => {
           className='flex items-center gap-2'
           onClick={() => {
             setActive("")
-            window.scrollTo(0,0)
+            window.scrollTo(0, 0)
           }}
         >
           <img src={logo} alt="logo" className='w-9 h-9 object-contain' />
           <p className='text-white text-[18px] font-bold cursor-pointer'>Huseynov Elmir</p>
         </Link>
+
+        <ul className='list-none hidden sm:flex flex-row gap-10'>
+          {navLinks.map((link) => (
+            <li key={link.id}
+              className={`${active === link.title
+                ? "text-white"
+                : "text-secondary"
+                } hover:text-white text-[18px] 
+                font-medium cursor-pointer`}
+              onClick={() => setActive(link.title)}>
+              <a href={`#${link.id}`}>
+                {link.title}
+              </a>
+            </li>
+          ))}
+        </ul>
+        <div className='sm:hidden flex flex-1  justify-end  items-center'>
+          <img src={toggle ? close : menu} alt="menu" className='w-[28px] h-[28px] object-contain cursor-pointer z-50'
+            onClick={() => setToggle(!toggle)}
+          />
+          <div className={`${!toggle ? "hidden" : "flex"}  bg-white absolute left-1 right-2 top-2 mx-4 my-2 w-[90%]  z-10 rounded-xl`}>
+            <ul className='list-none flex  justify-start items-start flex-col gap-4'>
+              {navLinks.map((link) => (
+                <li key={link.id}
+                  className={`${active === link.title
+                    ? "text-black"
+                    : "text-secondary"
+                    } hover:text-black text-[18px] 
+                font-medium cursor-pointer mx-4 my-2`}
+                  onClick={() => {
+                    setActive(link.title);
+                    setToggle(!toggle);
+
+                  }
+
+                  }>
+                  <a href={`#${link.id}`}>
+                    {link.title}
+                  </a>
+                </li>
+              ))}
+            </ul>
+          </div>
+        </div>
       </div>
     </nav>
   )
